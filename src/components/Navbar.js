14374a97ff533b45dc4f755102ea6da9447db186
jagratigupta07 <jagrati0304@gmail.com>
2023-03-04 23:34:24 +0530

    commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+export default function Navbar(props) {
+  return (
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+  <a className="navbar-brand" href="/">{props.title}</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">{props.aboutText}</a>
+      </li>
+      {/* <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a className="dropdown-item" href="/">Action</a>
+          <a className="dropdown-item" href="/">Another action</a>
+          <div className="dropdown-divider"></div>
+          <a className="dropdown-item" href="/">Something else here</a>
+        </div>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link disabled" href="/">Disabled</a>
+      </li> */}
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      {/* <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> */}
+    </form>
+    <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable DarkMode</label>
+                    </div>
+    {/* <div className={`form-check form-switch text=${props.mode==='light'?'dark':'light'}`} style={{color:props.mode==='dark'?'white':'black'}}>
+  <input className="form-check-input" type="checkbox" onClick={props.togglemode} role="switch" id="flexSwitchCheckChecked" checked/>
+  <label className="form-check-label" htmlFor="flexSwitchCheckChecked">Enable Dark Mode</label>
+</div> */}
+  </div>
+</nav>
+  )
+}
+
+Navbar.propTypes={title:propTypes.string.isRequired,
+  aboutText:propTypes.string}
+
+  Navbar.defaultProps={
+    title:'set title here',
+    aboutText:'about text here'
+  }
